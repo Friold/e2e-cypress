15726ecd5aa413ff0eccbba1037b9e1c6a763d50
Lexanne Painter <me@thelexanne.com>
2020-01-13 18:03:57 -0500

    Add Cypress Tests

---
+++ cypress/integration/integrations-e2e/before-Each.spec.js
@@ -0,0 +1,39 @@
+describe('Text box with max characters', () => {
+    beforeEach(() => {
+        cy.visit('/example-3');
+
+        cy.get('[data-cy="last-name-chars-left-count"]')
+            .as('charsLeftSpan');
+
+        cy.get('[data-cy="input-last-name"]')
+            .as('charInput');
+    });
+
+    it('displays the appropriate remaining characterS count', () => {
+
+
+        cy.get('@charsLeftSpan')
+        .invoke('text')
+        .should('equal', '15');
+
+        cy.get('@charInput').type('hello');
+
+        cy.get('@charsLeftSpan')
+        .invoke('text')
+        .should('equal', '10');
+
+        cy.get('@charInput').type(' my friend');
+
+        cy.get('@charsLeftSpan')
+        .invoke('text')
+        .should('equal', '0');
+    });
+
+    it('prevents the user from typing more characters once max is exceeded', () =>{
+
+        cy.get('@charInput').type('1234kdlskdjfikelwjfiodnafkojnaklfea');
+        cy.get('@charInput')
+            .should('have.attr', 'value', '1234kdlskdjfike');
+    });
+
+});
